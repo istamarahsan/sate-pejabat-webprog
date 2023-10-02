9d9846873393ca619c0df4be7de8ce6c9202881f
Tix <tix.personal@gmail.com>
2023-10-02 18:03:04 +0700

    best thing ever made today

--- resources/views/admin/test.blade.php
+++ resources/views/admin/test.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.dashboard')
 
+{{-- do as you wish with this file, i have won the war --}}
 @section('child-content')
     <h1 class="text-3xl font-bold">This is the Page Title</h1>
     <p class="mt-4">Page content goes here...</p>
