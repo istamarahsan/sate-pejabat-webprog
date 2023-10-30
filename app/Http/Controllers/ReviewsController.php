866f5dfd1e2a61af7f547356672a456989af0765
istamarahsan <istamarahsan@gmail.com>
2023-10-30 23:40:48 +0700

    fix review scale

--- app/Http/Controllers/ReviewsController.php
+++ app/Http/Controllers/ReviewsController.php
@@ -12,40 +12,41 @@ class ReviewsController extends Controller
 
     public function __construct()
     {
-        $this->reviewService = app('reviewService');
+        $this->reviewService = app("reviewService");
     }
 
     public function index(Request $request)
     {
         $reviews = $this->reviewService->getReviews();
-        return view('admin.reviews.index', [
-            'reviews' => $reviews,
+        return view("admin.reviews.index", [
+            "reviews" => $reviews,
         ]);
     }
 
-    public function create() {
-        return view('add-review');
+    public function create()
+    {
+        return view("add-review");
     }
 
     public function store(Request $request)
     {
         $details = $request->validate([
-            'name' => ['string', 'required'],
-            'taste' => ['int', 'required'],
-            'atmosphere' => ['int', 'required'],
-            'cleanliness' => ['int', 'required'],
-            'service' => ['int', 'required'],
-            'price' => ['int', 'required'],
-            'comments' => ['string', 'required'],
-            'goals' => ['string', 'required']
+            "name" => ["string", "required"],
+            "taste" => ["int", "required"],
+            "atmosphere" => ["int", "required"],
+            "cleanliness" => ["int", "required"],
+            "service" => ["int", "required"],
+            "price" => ["int", "required"],
+            "comments" => ["string", "required"],
+            "goals" => ["string", "required"],
         ]);
 
         $reviewId = $this->reviewService->createReview($details);
 
         if ($reviewId == null) {
-            return response('no', 500);
+            return response("no", 500);
         }
 
-        return '<span>Thank you for leaving a review, ' . $details['name'] . '.</span>';
+        return "<span>Thank you for leaving a review, " . $details["name"] . ".</span>";
     }
 }
--- app/Lib/ReviewService.php
+++ app/Lib/ReviewService.php
@@ -7,7 +7,6 @@ use Illuminate\Support\Facades\DB;
 
 class ReviewService
 {
-
     /**
      * @return array [
      * id: int,
@@ -24,10 +23,12 @@ class ReviewService
      */
     public function getReviews(): array
     {
-        return DB::table('reviews')
-            ->selectRaw('id, date, reviewer_name AS name, score_taste AS taste, score_atmosphere AS atmosphere, score_cleanliness AS cleanliness, score_service AS service, score_price AS price, reviewer_comments AS comments, reviewer_goals AS goals')
+        return DB::table("reviews")
+            ->selectRaw(
+                "id, date, reviewer_name AS name, score_taste AS taste, score_atmosphere AS atmosphere, score_cleanliness AS cleanliness, score_service AS service, score_price AS price, reviewer_comments AS comments, reviewer_goals AS goals",
+            )
             ->get()
-            ->map(fn ($e) => get_object_vars($e))
+            ->map(fn($e) => get_object_vars($e))
             ->toArray();
     }
 
@@ -43,30 +44,38 @@ class ReviewService
      * goals: string
      * ]
      */
-    public function createReview($details): int | null
+    public function createReview($details): int|null
     {
         if (
-            $this->array_any(function ($e) {
-                return $e < 1 || $e > 3;
-            }, [$details['taste'], $details['atmosphere'], $details['cleanliness'], $details['service'], $details['price']])
+            $this->array_any(
+                function ($e) {
+                    return $e < 1 || $e > 6;
+                },
+                [
+                    $details["taste"],
+                    $details["atmosphere"],
+                    $details["cleanliness"],
+                    $details["service"],
+                    $details["price"],
+                ],
+            )
         ) {
             return null;
         }
 
         $now = Carbon::now();
 
-        return DB::table('reviews')
-            ->insertGetId([
-                'date' => $now,
-                'reviewer_name' => $details['name'],
-                'score_taste' => $details['taste'],
-                'score_atmosphere' => $details['atmosphere'],
-                'score_cleanliness' => $details['cleanliness'],
-                'score_service' => $details['service'],
-                'score_price' => $details['price'],
-                'reviewer_comments' => $details['comments'],
-                'reviewer_goals' => $details['goals']
-            ]);
+        return DB::table("reviews")->insertGetId([
+            "date" => $now,
+            "reviewer_name" => $details["name"],
+            "score_taste" => $details["taste"],
+            "score_atmosphere" => $details["atmosphere"],
+            "score_cleanliness" => $details["cleanliness"],
+            "score_service" => $details["service"],
+            "score_price" => $details["price"],
+            "reviewer_comments" => $details["comments"],
+            "reviewer_goals" => $details["goals"],
+        ]);
     }
 
     private function array_any(callable $fn, array $array)
