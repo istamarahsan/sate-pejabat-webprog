e9096ddb5a5b983a265af25d642c969726fe1f87
istamarahsan <istamarahsan@gmail.com>
2023-12-08 13:55:50 +0700

    review redirects if logged in

--- resources/views/staff/home.blade.php
+++ resources/views/staff/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <div class="p-4 font-black flex flex-col gap-4">
-        <p class="text-3xl">Welcome whoever that is.</p>
+        <p class="text-3xl">Welcome, S{{ auth()->user()->id }}</p>
         <a class="btn btn-primary max-w-fit" type="button" href="{{ route('staff.createtransaction') }}">
             <p>Want to make Transaction?</p>
         </a>
